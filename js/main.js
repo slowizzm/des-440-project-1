bba20a7c3de7ff1ef99841f716397b479f01371d
slow_izzm <gene33@live.missouristate.edu>
2017-08-28 20:27:09 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Anything here will be processed by the javascript interpreter when this file finishes loading. */
+
+
+
+$(document).ready(function(){
+/* Anything here will run when the entire HTML document has been processed by the browser */
+
+
+});
